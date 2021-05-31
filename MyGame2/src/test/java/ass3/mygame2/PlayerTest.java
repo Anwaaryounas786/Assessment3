e06ddb361472877db0c069268d83a4c524f7962e
Anwaaryounas786 <82020095+Anwaaryounas786@users.noreply.github.com>
2021-05-31 20:16:22 +1000

    uploading new files

---
+++ MyGame2/src/test/java/ass3/mygame2/PlayerTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Anwaar
+ */
+public class PlayerTest {
+    Player p;
+    Item item1;
+    public PlayerTest() {
+        p=new Player();
+        item1=new Item("Item test","This is a test",100);
+    }
+
+    /**
+     * Test of check inventory method, of class Player.
+     */
+    @Test
+    public void testCheckinventory() {
+        p.addItemInventory(item1);
+        int expected=1;
+        int actual=p.checkinventory(item1);
+        assertEquals(expected,actual);
+    }
+    
+}
---
+++ MyGame2/src/test/java/ass3/mygame2/RoomCreationTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Anwaar
+ */
+public class RoomCreationTest {
+    
+    RoomCreation c;
+    
+    public RoomCreationTest() {
+        c=new RoomCreation();
+    }
+
+    /**
+     * Test of checkitemroom method, of class RoomCreation.
+     */
+    @Test
+    public void testCheckitemroom() {
+        String expected=null;
+        String actual=c.checkitemroom();
+        System.out.println(actual);
+    }
+    
+}
